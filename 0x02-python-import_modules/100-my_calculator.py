153351e8bdf6bd2548ad854e3b1cf41b8c2358ad
Anana99 <ananaitohoinyene99@gmail.com>
2023-10-05 09:09:20 -0700

    Update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 
+import operator
+import sys
+
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
-    import sys
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    ops = {"+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.truediv}
+
+    if sys.argv[2] not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+    try:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+    except ValueError:
+        print("Invalid input. Please provide valid integer values for <a> and <b>")
+        sys.exit(1)
+
     print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
